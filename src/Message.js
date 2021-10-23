4398e208f808d1877cd3c236978ce2f3b0229555
Andre Colon <andrecolon@gmail.com>
2021-10-23 16:43:08 -0700

    Feature - Added Chat per CHannel

--- src/Message.js
+++ src/Message.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import './Message.css';
-import { Avatar } from '@material-ui/core';
+import React from "react";
+import "./Message.css";
+import { Avatar } from "@material-ui/core";
 
-function Message() {
-    return (
-        <div className="message">
-            <Avatar />
-            <div className="message__info">
-                <h4>Andre is Here 
-                    <span className="message__timeStamp">
-                        this is a timestamp
-                    </span>
-                </h4>
-                <p>this is a message</p>
-            </div>
-        </div>
-    )
+function Message({ timestamp, user, message }) {
+  return (
+    <div className="message">
+      <Avatar src={user.photo} />
+      <div className="message__info">
+        <h4>
+          {user.displayName}
+          <span className="message__timeStamp">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Message
+export default Message;
